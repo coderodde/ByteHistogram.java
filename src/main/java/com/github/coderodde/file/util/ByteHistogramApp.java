6f10e43b5a41ebc26de78a5bf1b7bc4070189609
Rodion Efremov <coderodd3@yandex.com>
2024-11-17 12:17:45 +0200

    Version 1.0.2.

--- src/main/java/com/github/coderodde/file/util/ByteHistogramApp.java
+++ src/main/java/com/github/coderodde/file/util/ByteHistogramApp.java
@@ -1,164 +1,103 @@
 package com.github.coderodde.file.util;
 
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class implements a program for counting byte histograms in files.
  * 
- * @version 1.0.0 (Nov 13, 2024)
+ * @version 1.0.2 (Nov 17, 2024)
  * @since 1.0.0 (Nov 13, 2024)
  */
 public final class ByteHistogramApp {
+    
+    private static final Logger LOGGER = 
+            Logger.getLogger(ByteHistogram.class.getSimpleName());
 
     public static void main(String[] args) {
-        List<InputStream> inputStreamList = null;
-        
-        // Prepare the input streams from which to build the (shared) byte 
-        // histogram:
-        try {
-            inputStreamList = getInputStreams(args);
-        } catch (final PairException ex) {
-            
-            ex.getFileNotFoundException()
-              .getExceptionList()
-              .forEach((e) -> System.err.println(e.getMessage()));
-            
-            ex.getIOException()
-              .getExceptionList()
-              .forEach((e) -> System.err.println(e.getMessage()));
-            
-            System.exit(-1);
-        }
-        
-        // Once here, we have valid input streams. Request the histogram and 
-        // print it in the console:
-        try {
-            System.out.println(processInputStreamList(inputStreamList));
-        } catch (MultipleIOException ex) {
-            
-            ex.getExceptionList()
-              .forEach((e) -> System.err.println(e.getMessage()));
-            
-            System.exit(-2);
-        }
-    }
-    
-    /**
-     * Converts the input argument list to the list of input streams.
-     * 
-     * @param args the names of the files to process.
-     * 
-     * @return the input stream list.
-     * 
-     * @throws MultipleFileNotFoundException if any file failed.
-     */
-    private static List<InputStream> getInputStreams(final String[] args)
-            throws PairException {
+        final ByteHistogram byteHistogram = new ByteHistogram();
         
         if (args.length == 0) {
-            return List.of(System.in);
-        }
-        
-        final List<InputStream> inputStreamList = new ArrayList<>(args.length);
-        
-        final MultipleFileNotFoundException exceptionListFileNotFound = 
-                new MultipleFileNotFoundException();
-        
-        final MultipleIOException exceptionListIO =
-                new MultipleIOException();
-        
-        for (final String fileName : args) {
             try {
-                final InputStream is = new FileInputStream(new File(fileName));
-                inputStreamList.add(is);
-            } catch (final FileNotFoundException ex) {
-                // Add the exception ex to the exceptionList:
-                exceptionListFileNotFound.add(ex);
-                
-                for (final InputStream is : inputStreamList) {
-                    try {
-                        is.close();
-                    } catch (final IOException ioException) {
-                        exceptionListIO.add(ioException);
-                    }
+                loadByteHistogramFromStdin(byteHistogram);
+            } catch (IOException ex) {
+                LOGGER.log(
+                        Level.SEVERE, 
+                        "Could not load the histogram from " + 
+                        "standard input stream.");
+            }
+        } else {
+            for (final String fileName : args) {
+                try {
+                    loadByteHistogramFromFile(fileName, byteHistogram);
+                } catch (IOException ex) {
+                    LOGGER.log(
+                            Level.SEVERE,
+                            "Could not load the histogram from file \"{0}\".", 
+                            fileName);
                 }
             }
         }
         
-        if (!exceptionListIO.isEmpty() || 
-            !exceptionListFileNotFound.isEmpty()) {
-            // Once here, something went wrong. Throw:
-            
-            throw new PairException(exceptionListFileNotFound,
-                                    exceptionListIO);
-        }
-        
-        return inputStreamList;
+        System.out.println(byteHistogram);
     }
     
     /**
-     * Builds the shared histogram from the input streams in the argument.
+     * Loads the byte histogram data only from standard input.
      * 
-     * @param inputStreamList the list of input stream supplying the bytes.
-     * 
-     * @return the shared byte histogram.
-     * 
-     * @throws MultipleIOException if any stream threw.
-     */
-    private static ByteHistogram
-         processInputStreamList(final List<InputStream> inputStreamList) 
-         throws MultipleIOException {
-             
-        final ByteHistogram histogram = new ByteHistogram();
-        final MultipleIOException ex = new MultipleIOException();
-        
-        for (final InputStream is : inputStreamList) {
-            try {
-                processInputStream(
-                        new BufferedInputStream(is), 
-                        histogram);
-                
-            } catch (final IOException e) {
-                // Add the new I/O exception e to ex::
-                ex.add(e);
-            }
-        }
-        
-        if (!ex.isEmpty()) {
-            // Once here, something went wrong. Throw:
-            throw ex;
-        }
-        
-        return histogram;
-    }
-         
-    /**
-     * Processes the input stream reading bytes from it until end of file is 
-     * reached.
-     * 
-     * @param is        the input stream.
-     * @param histogram the target histogram.
+     * @param byteHistogram the byte histogram for holding the statistics.
      * 
      * @throws IOException if I/O fails.
      */
-    private static void processInputStream(final InputStream is,
-                                           final ByteHistogram histogram) 
+    private static void loadByteHistogramFromStdin(
+            final ByteHistogram byteHistogram) throws IOException {
+        
+        loadImpl(new BufferedInputStream(System.in),
+                 byteHistogram);
+    }
+    
+    /**
+     * Loads the byte histogram data only from the file named {@code fileName}.
+     * 
+     * @param fileName      the name of the file to load from.
+     * @param byteHistogram the byte histogram for holding the statistics.
+     * 
+     * @throws FileNotFoundException if file named {@code fileName} is not 
+     *                               found.
+     * @throws IOException if I/O fails.
+     */
+    private static void loadByteHistogramFromFile(
+            final String fileName,
+            final ByteHistogram byteHistogram)
+            throws FileNotFoundException, IOException {
+        
+        final InputStream is =
+                new BufferedInputStream(
+                        new FileInputStream(fileName));
+        
+        loadImpl(is, byteHistogram);
+    }
+    
+    /**
+     * Performs the actual loading of the data to the input byte histogram.
+     * 
+     * @param is            the input stream from which to load the data.
+     * @param byteHistogram the target byte histogram.
+     * 
+     * @throws IOException if I/O fails.
+     */
+    private static void loadImpl(final InputStream is, 
+                                 final ByteHistogram byteHistogram) 
             throws IOException {
+        int ch;
         
-        int i;
-        
-        while ((i = is.read()) != -1) {
-            histogram.insert(i);
+        while ((ch = is.read()) != -1) {
+            byteHistogram.insert(ch);
         }
-        
-        is.close();
     }
 }
-
